d91d92d6a23a113db3df97b7acc3652dc359b51f
Rafael Freitas <rafaelborges1985@hotmail.com>
2021-06-29 08:16:17 -0300

    Set up typescript, html and styles

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,23 @@
-// Code goes here!
+class ProjectInput {
+  templateEl: HTMLTemplateElement;
+  hostEl: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    this.templateEl = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostEl = document.getElementById("app")! as HTMLDivElement;
+
+    const importNode = document.importNode(this.templateEl.content, true);
+    this.element = importNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input'
+    this.attach();
+  }
+
+  private attach() {
+    this.hostEl.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const projInput = new ProjectInput();
