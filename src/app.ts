f3f30643bd00f2b2ce82ee8db4e44e036199e479
Rafael Freitas <rafaelborges1985@hotmail.com>
2021-07-25 20:11:02 -0300

    Finish decorator and reusable validate function

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,47 @@
+// Validation
+
+interface Validatable {
+  value: string | number;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+function validate(config: Validatable): boolean {
+  let isValid = true;
+  if (config.required) {
+    isValid = isValid && config.value.toString().trim().length > 0;
+  }
+  if(config.minLength != null && typeof config.value === 'string') {
+    isValid = isValid && config.value.length > config.minLength;
+  }
+  if(config.maxLength != null && typeof config.value === 'string') {
+    isValid = isValid && config.value.length < config.maxLength;
+  }
+  if(config.min != null && typeof config.value === 'number') {
+    isValid = isValid && config.value > config.min;
+  }
+  if(config.max != null && typeof config.value === 'number') {
+    isValid = isValid && config.value > config.max;
+  }
+  return isValid;
+}
+
+// Autobind decorator
+
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
 class ProjectInput {
   templateEl: HTMLTemplateElement;
   hostEl: HTMLDivElement;
@@ -15,20 +59,68 @@ class ProjectInput {
 
     const importNode = document.importNode(this.templateEl.content, true);
     this.element = importNode.firstElementChild as HTMLFormElement;
-    this.element.id = 'user-input'
+    this.element.id = "user-input";
+
+    this.configure();
     this.attach();
 
-    this.titleInputEl = document.querySelector('#title')! as HTMLInputElement;
-    this.descriptionInputEl = document.querySelector('#description')! as HTMLInputElement;
-    this.peopleInputEl = document.querySelector('#people')! as HTMLInputElement;
+    this.titleInputEl = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionInputEl = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleInputEl = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
   }
 
+  private gatherUserInput(): [string, string, number] | void {
+    const title = this.titleInputEl.value;
+    const description = this.descriptionInputEl.value;
+    const people = this.peopleInputEl.value;
+
+    const inputValues: [string, string, number] = [
+      title,
+      description,
+      +people,
+    ];
+
+    if (
+      !validate({ value: title, required: true }) ||
+      !validate({ value: description, required: true, minLength: 5 }) ||
+      !validate({ value: +people, required: true, min: 1 })
+    ) {
+      alert("Invalid inputs");
+      return;
+    }
+
+    return inputValues;
+  }
+
+  @autobind
   private submitHandler(e: Event) {
     e.preventDefault();
+
+    const userInput = this.gatherUserInput();
+
+    if (Array.isArray(userInput)) {
+      const [title, description, people] = userInput;
+      console.log(
+        `Title: ${title}, Description: ${description}, People: ${people}`
+      );
+      this.clearInputs();
+    }
+  }
+
+  private clearInputs() {
+    this.titleInputEl.value = "";
+    this.descriptionInputEl.value = "";
+    this.peopleInputEl.value = "";
   }
 
   private configure() {
-    this.element.addEventListener('click', this.submitHandler.bind(this))
+    this.element.addEventListener("submit", this.submitHandler);
   }
 
   private attach() {
